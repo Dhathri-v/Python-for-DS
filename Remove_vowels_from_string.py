42cb4eba581bc498d39b97af3912e68382ade2a7
Dhathri-v <96650228+Dhathri-v@users.noreply.github.com>
2022-01-07 22:20:35 +0530

    Update Remove_vowels_from_string.py

--- Remove_vowels_from_string.py
+++ Remove_vowels_from_string.py
@@ -1,6 +1,14 @@
 s=str(input())
-v="AEIOUaeiou"
+vowels="AEIOUaeiou"
 for i in s:
     if i in v:
         s=s.replace(i,"")
+print(s)
+
+#OR
+
+vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+for i in s:
+    if i in vowels:
+        s = s.replace(i, "")
 print(s)
